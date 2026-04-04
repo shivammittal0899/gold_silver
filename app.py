e4367c0ef3362f4df7934bf88e9b38448ae8ac93
Shivam Mittal <shivammittal@Shivams-MacBook-Pro.local>
2026-04-05 00:19:08 +0530

    new

--- app.py
+++ app.py
@@ -386,7 +386,7 @@ def trailing_worker(task_id, instrument, indicator, timeframe, qty, min_val, mul
                 break
             
             
-            now = datetime.now()# + timedelta(hours=5, minutes=30)
+            now = datetime.now() + timedelta(hours=5, minutes=30)
             # now = datetime.now() 
             log1(f'Present Time: {now}')
 
@@ -826,7 +826,7 @@ def analysis_worker(tf, instrument, instrument_token):
 
 
         try:
-            now = datetime.now() + timedelta(hours=5, minutes=30)
+            now = datetime.now()# + timedelta(hours=5, minutes=30)
             # now = datetime.now() 
             log1(f'Present Time: {now} --- {datetime.now()}')
 

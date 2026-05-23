89ffce14f12917258f83a97dc2ec1836176ac67f
Shivam Mittal <shivammittal@Shivams-MacBook-Pro.local>
2026-05-23 21:41:09 +0530

    new

--- delivery_data.py
+++ delivery_data.py
@@ -1,64 +1,5 @@
 import sqlite3
 import pandas as pd
-# def init_delivery_db():
-#     conn = sqlite3.connect(
-#         "delivery_history.db"
-#     )
-#     cursor = conn.cursor()
-#     cursor.execute("""
-#         CREATE TABLE IF NOT EXISTS delivery_history (
-#             date TEXT,
-#             symbol TEXT,
-#             ttl_trd_qnty INTEGER,
-#             turnover_lacs REAL,
-#             no_of_trades INTEGER,
-#             deliv_qty INTEGER,
-#             deliv_per REAL,
-#             PRIMARY KEY (
-#                 date,
-#                 symbol
-#             )
-#         )
-#     """)
-#     conn.commit()
-#     conn.close()
-# def update_delivery_data(df):
-#     conn = sqlite3.connect(
-#         "delivery_history.db"
-#     )
-#     cursor = conn.cursor()
-#     # CLEAN
-#     df.columns = [
-#         c.strip()
-#         for c in df.columns
-#     ]
-#     for _, row in df.iterrows():
-#         try:
-#             cursor.execute("""
-#                 INSERT OR REPLACE INTO delivery_history (
-#                     date,
-#                     symbol,
-#                     ttl_trd_qnty,
-#                     turnover_lacs,
-#                     no_of_trades,
-#                     deliv_qty,
-#                     deliv_per
-#                 )
-#                 VALUES (?, ?, ?, ?, ?, ?, ?)
-#             """, (
-#                 row["DATE1"],
-#                 row["SYMBOL"].strip(),
-#                 int(row["TTL_TRD_QNTY"]),
-#                 float(row["TURNOVER_LACS"]),
-#                 int(row["NO_OF_TRADES"]),
-#                 int(row["DELIV_QTY"]),
-#                 float(row["DELIV_PER"])
-#             ))
-#         except Exception as e:
-#             print(e)
-#     conn.commit()
-#     conn.close()
-#     print("✅ Delivery data updated")
 
 def init_delivery_db():
     conn = sqlite3.connect(
@@ -76,8 +17,6 @@ def init_delivery_db():
             no_of_trades INTEGER,
             deliv_qty INTEGER,
             deliv_per REAL,
-            volume_ratio REAL,
-            delivery_score REAL,
             PRIMARY KEY (
                 date,
                 symbol
@@ -148,21 +87,21 @@ def update_delivery_data_file(df):
     # VOLUME RATIO
     # ====================================
 
-    df["volume_ratio"] = (
+    # df["volume_ratio"] = (
 
-        df["TTL_TRD_QNTY"]
+    #     df["TTL_TRD_QNTY"]
 
-        /
+    #     /
 
-        df["TTL_TRD_QNTY"].mean()
+    #     df["TTL_TRD_QNTY"].mean()
 
-    )
+    # )
 
     # ====================================
     # DELIVERY SCORE
     # ====================================
 
-    df["delivery_score"] = (df["DELIV_PER"] * 0.6 + df["volume_ratio"] * 40)
+    # df["delivery_score"] = (df["DELIV_PER"] * 0.6 + df["volume_ratio"] * 40)
 
     # ====================================
     # SAVE
@@ -184,9 +123,7 @@ def update_delivery_data_file(df):
                     turnover_lacs,
                     no_of_trades,
                     deliv_qty,
-                    deliv_per,
-                    volume_ratio,
-                    delivery_score
+                    deliv_per
 
                 )
 
@@ -210,11 +147,7 @@ def update_delivery_data_file(df):
 
                 int(row["DELIV_QTY"]),
 
-                float(row["DELIV_PER"]),
-
-                float(row["volume_ratio"]),
-
-                float(row["delivery_score"])
+                float(row["DELIV_PER"])
 
             ))
 

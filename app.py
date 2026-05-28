301cc19837c5e6f732c94c644e3f05b4326df6ac
Shivam Mittal <shivammittal@Shivams-MacBook-Pro.local>
2026-05-28 22:41:56 +0530

    new

--- app.py
+++ app.py
@@ -4535,7 +4535,7 @@ def get_nearby_options(index_name="NIFTY"):
 
     kite_local = KiteConnect(api_key=API_KEY)
     kite_local.set_access_token(access_token)
-
+    quote = kite_local.quote([str(nifty_token)])
     log1("inside nearby options")
     df = get_weekly_options(kite_local, index_name)
     log1(f"weekly options fetched {df}")
@@ -4547,7 +4547,7 @@ def get_nearby_options(index_name="NIFTY"):
     niftybank_token = INSTRUMENT_MAP.get("NIFTY BANK")
     log1(f"nifty_token ---- {nifty_token}")
 
-    quote = kite_local.quote([str(nifty_token)])
+    
     log1(quote)
 
     spot = quote[str(nifty_token)]["last_price"]
